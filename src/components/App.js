76ce9a3397af86f793db0806b141ca2d91534c4c
SatYu26 <goyalsatyam8@gmail.com>
2021-03-26 03:25:53 +0530

    Injecting an index

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super();
     this.state = {
       myAppointments: [],
+      lastIndex: 0,
     };
   }
 
@@ -18,6 +19,8 @@ class App extends Component {
       .then((response) => response.json())
       .then((result) => {
         const apts = result.map((item) => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
           return item;
         });
         this.setState({
