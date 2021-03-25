5818c1c4847270f4095f87b7a92a01299c10bb10
SatYu26 <goyalsatyam8@gmail.com>
2021-03-26 03:15:39 +0530

    Using a render template expression

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 
 class ListAppointments extends Component {
   render() {
-    const ListItems = this.props.appointments.map((item) => {
-      return (
-        <div>
-          <div>{item.petName}</div>
-          <div>{item.ownerName}</div>
-        </div>
-      );
-    });
+    return (
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map((item) => {
+          return (
+            <div className="pet-item col media py-3">
+              <div className="mr-3">
+                <button className="pet-delete btn btn-sm btn-danger">X</button>
+              </div>
 
-    return <div>{ListItems}</div>;
+              <div className="pet-info media-body">
+                <div className="pet-head d-flex">
+                  <span className="pet-name">{item.petName}</span>
+                  <span className="apt-date ml-auto">{item.aptDate}</span>
+                </div>
+
+                <div className="owner-name">
+                  <span className="label-item">Owner: </span>
+                  <span>{item.ownerName}</span>
+                </div>
+                <div className="apt-notes">{item.aptNotes}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
