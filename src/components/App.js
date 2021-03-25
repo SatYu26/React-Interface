3902adc22d711c21cb7ba5cc842895ddb71a74cd
SatYu26 <goyalsatyam8@gmail.com>
2021-03-26 00:52:01 +0530

    State and expression basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/App.css";
 
 import AddAppointments from "./AddAppointments.js";
 import ListAppointments from "./ListAppointments.js";
 import SearchAppointments from "./SearchAppointments.js";
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myName: "Satyam",
+    };
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
